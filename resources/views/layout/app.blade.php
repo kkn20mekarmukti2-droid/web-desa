bcbb5f629d5924854ecad660c230300489921f40
kkn20mekarmukti2-droid <email-kkn20mekarmukti2-droid@example.com>
2025-08-25 18:58:54 +0700

    feat: Add Instagram link for KKN Universitas Muhammadiyah Bandung
    - Added Instagram link @kkn20mekarmukti2 for KKN UMB
    - Completed social media integration for both KKN teams
    - Footer now has clickable links to both Instagram accounts
    - Professional social media presence in website footer

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -210,7 +210,7 @@
                 Designed by 
                 <a href="https://www.instagram.com/kkn_mekarmuktiplb/">KKN Politeknik LP3I Bandung</a> 
                 & 
-                <a href="#">KKN Universitas Muhammadiyah Bandung</a>
+                <a href="https://www.instagram.com/kkn20mekarmukti2/">KKN Universitas Muhammadiyah Bandung</a>
                 <br>
                 <small class="text-muted">Kelompok 20 Mekarmukti (Kelompok ke-2) • 2024</small>
             </div>
